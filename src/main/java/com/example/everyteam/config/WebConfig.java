cccd3bd2ace2ea176e590e010d0b10cedac94e35
hweyoung <gnldud0516@gmail.com>
2022-11-18 21:54:50 +0900

    feat : amand cors

--- src/main/java/com/example/everyteam/config/WebConfig.java
+++ src/main/java/com/example/everyteam/config/WebConfig.java
@@ -6,15 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:3001")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true)
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .maxAge(3000);
     }
-
 }
