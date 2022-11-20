15a8c7f0b52e51741db9687ad2ec30b041384b68
hweyoung <gnldud0516@gmail.com>
2022-11-20 10:42:48 +0900

    feat : update git ignore

--- src/main/java/com/example/everyteam/config/exception/ErrorResponseStatus.java
+++ src/main/java/com/example/everyteam/config/exception/ErrorResponseStatus.java
@@ -26,7 +26,8 @@ public enum ErrorResponseStatus {
     EXIST_USER_IN_TEAM(false, 4007, "이미 team에 등록된 유저입니다."),
     MEET_CODE_ERROR(false, 4008, "잘못된 meet code 입니다."),
     MEET_DATE_ERROR(false, 4009, "잘못된 날짜를 입력했습니다."),
-
+    MEET_POST_ERROR(false, 4009, "Post에서 찾을 수 없는 MeetCode입니다."),
+    MEET_USER_NOT_FOUND(false, 4009, "Meet에 등록되지 않은 유저입니다."),
 
     //5000 : Server connection 오류
     SERVER_ERROR(false, 5000, "서버와의 연결에 실패하였습니다."),
